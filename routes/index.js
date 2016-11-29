c85f6d779ffa14db9ce609f4265410f464da4592
sdweber422 <sdweber422@gmail.com>
2016-11-29 14:08:57 -0800

    Setup initial database calls, routes, and view to test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Books = require('../database/db').Books
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const page = req.body.page || 2
+  const offset = (page - 1) * 10
+  return Books.all(offset)
+.then( books => {
+  console.log(books)
+  res.render('index', { title: 'Express', books: books })
+})
 });
+
+router.post('/details', function(req, res, next) {
+
+})
 
 module.exports = router;
