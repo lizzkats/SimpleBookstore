ba6edd7be529981286d915a2ea31ace561475c35
mikeadossi <mikeadossi@gmail.com>
2016-11-30 11:52:20 -0800

    add authors and genres to index.js and display on homepage

--- database/db.js
+++ database/db.js
@@ -6,10 +6,28 @@ const db = pgp(connectionString)
 const getAllBooks = 'SELECT * FROM books LIMIT 10 OFFSET $1'
 
 const getBook = 'SELECT * FROM books WHERE id = $1'
-
+const getAuthors = 'SELECT authors.*, book_id FROM authors JOIN book_authors ON id = author_id WHERE book_id IN ($1:csv)'
+const getGenres = 'SELECT genres.*, book_id FROM genres JOIN book_genres ON id = genre_id WHERE book_id IN ($1:csv)'
 const Books = {
   all: (offset) => db.any(getAllBooks, [offset])
-                  .then(),
+                  .then(books => {
+                    const bookList = books
+                    const bookIds = books.map(book => book.id)
+                    if(bookIds.length === 0){
+                      return Promise.resolve(books)
+                    }
+                    Promise.all([Authors.get(bookIds), Genres.get(bookIds), bookList])
+                    .then(results => {
+                      const authors = results[0]
+                      const genres = results[1]
+                      const books = results[2]
+                      books.forEach(book => {
+                        book.authors = authors.filter(author => author.book_id === book.id)
+                        book.genres = genres.filter(genre => genre.book_id === book.id)
+                      })
+                      return books
+                    })
+                  }),
   get: (id) => db.one(getBook, [id]),
   delete: (id) => db.one(deleteBook, [id]),
   add: (description, image_url, title) => db.one(addBook, [description, image_url, title]),
